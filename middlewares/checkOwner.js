4eeaed2be600e9e704021ffa62f8540c4e96f199
NirwanArr <arrachmann@gmail.com>
2023-10-22 19:50:57 +0700

    Upload Diagram

--- middlewares/checkOwner.js
+++ middlewares/checkOwner.js
@@ -1,9 +1,14 @@
-const ApiError = require("../utils/apiError")
+const ApiError = require("../utils/apiError");
 
 const checkOwner = (req, res, next) => {
-	if (req.user.id != req.params.id)
-		return next(new ApiError("kamu bukan pemilik akun ini", 401))
-	next()
-}
+    if (req.user.id != req.params.id)
+        return next(
+            new ApiError(
+                "kamu bukan pemilik akun ini",
+                401
+            )
+        );
+    next();
+};
 
 module.exports = checkOwner;
